64c55c43615982dbe33c410102c9d83d07dce4ce
antongoransson <anton.goransson95@gmail.com>
2018-06-08 17:24:37 +0200

    Update app to correct routes

--- src/app/App.js
+++ src/app/App.js
@@ -1,45 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-const Home = () => (
+const Overview = () => (
   <div>
-    <h2>Home</h2>
+    <h2>Overview</h2>
   </div>
 );
 
-const About = () => (
+const HandleItems = () => (
   <div>
-    <h2>About</h2>
-  </div>
-);
-
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-);
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>Components</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.url}/:topicId`} component={Topic} />
-    <Route
-      exact
-      path={match.url}
-      render={() => <h3>Please select a topic.</h3>}
-    />
+    <h3>Handle items</h3>
   </div>
 );
 
@@ -48,21 +18,15 @@ const App = () => (
     <div>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">Overview</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/topics">Topics</Link>
+          <Link to="/handle-items">Handle Items</Link>
         </li>
       </ul>
-
       <hr />
-
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
+      <Route exact path="/" component={Overview} />
+      <Route path="/handle-items" component={HandleItems} />
     </div>
   </Router>
 );
